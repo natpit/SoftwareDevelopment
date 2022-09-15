b838b8770dbe003545d6d98b8a373647fbdc616f
natpit <57505976+natpit@users.noreply.github.com>
2022-09-15 14:42:17 +0300

    Add files via upload

---
+++ ConsoleMVC/ConsoleMVC/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleMVC.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TipCalculatorController tipCalculatorController = new TipCalculatorController();
+        }
+    }
+}
